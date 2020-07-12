01ea8a3dfb41dca87aae77d4359245c9deb9a26c
federico <federico@federicos-MacBook.local>
2020-07-12 16:22:52 +0200

    Remove files argument

--- invert.py
+++ invert.py
@@ -14,27 +14,27 @@ from PIL import Image, ImageOps
     default=100,
     help='Quality of the inverted image. [1-100]'
 )
-@click.option(
-    '-p',
-    '--path',
-    type=click.Path(),
-    default=None,
-    help='Invert all pictures at given path.'
-)
 @click.argument(
-    'files',
+    'paths',
     nargs=-1,
     type=click.Path()
 )
-def invert(quality, path, files):
-    if path:
-        files = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
-    for filename in files:
-        if filename.lower().endswith(('.jpeg', '.jpg', '.png')):
-            print(f'Inverting {filename}')
-            image = Image.open(filename)
-            image_invert = ImageOps.invert(image.convert('RGB'))
-            image_invert.save(filename, quality=quality)
+def invert(quality, paths):
+    for path in paths:
+        if isfile(path):
+            _invert(quality, path)
+        else:
+            files = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
+            for filename in files:
+                _invert(quality, filename)
+
+
+def _invert(quality, filename):
+    if filename.lower().endswith(('.jpeg', '.jpg', '.png')):
+        print(f'Inverting {filename}')
+        image = Image.open(filename)
+        image_invert = ImageOps.invert(image.convert('RGB'))
+        image_invert.save(filename, quality=quality)
 
 
 if __name__ == '__main__':
